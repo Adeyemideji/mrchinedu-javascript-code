3947555a09abe3d4b56430133ac2d69922dcb95b
Adeyemideji <adedejiadeyemi627@gmail.com>
2024-05-06 10:22:04 -0800

    my javascript code

---
+++ forloop2.js
@@ -0,0 +1,13 @@
+const arr = [6,4,5,6,8];
+	console.log(arr[0]);
+let user = {
+
+	name: "deji",
+	age: 18,
+	cohort: 20,
+	sex: "male"
+};
+
+for(let detail in user){
+	console.log(user[detail]); 	
+}
---
+++ sum.js
@@ -0,0 +1,11 @@
+var a = 10;
+var b = 20;
+let c = (a + b);
+value = 2 * c
+console.log(value);
+
+var x = 500;
+let y = x % 10;
+let z = y + x;
+console.log(z);
+
